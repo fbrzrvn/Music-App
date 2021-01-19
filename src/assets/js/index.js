bd0dbaa5448ba9c53de55f02d01ef0bccef2d72d
fabrizio <faber.rvn@gmail.com>
2021-01-19 12:23:31 +0100

    #3 Fetch data from api

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,15 @@
 import { render as renderForm } from "./SearchForm.js";
+import { getData } from './api.js';
 
 renderForm();
+
+
+const data = {
+  type: "song",
+  country: "",
+  explicit: "",
+  limit: "5",
+  searchQuery: "bob marley",
+};
+
+getData(data);
